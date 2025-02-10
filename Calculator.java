bcc3073a252105d68947d1c5fdcb192cb6513286
SHUBHAM <SHUBHAMWADHANKAR123@GMAIL.COM>
2025-02-10 16:51:24 +0530

    java file added

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+public class Calculator {
+    // Addition method
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtraction method
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiplication method
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Division method (handles division by zero)
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }
+
+    // Main method for manual testing
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
+}
